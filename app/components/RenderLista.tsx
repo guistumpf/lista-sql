25a2d908037d372c97f1baf43f8e11c0b99e92ac
João Guilherme Stumpf Fernandes <guist4@proton.me>
2026-06-04 10:23:32 -0300

    order by

--- app/components/RenderLista.tsx
+++ app/components/RenderLista.tsx
@@ -5,7 +5,7 @@ import { db } from "../db";
 import RealList from "./MapLista";
  
 export default async function Lista() {
-    const rows = await db.execute(sql`SELECT texto, id FROM "Tarefas"`)
+    const rows = await db.execute(sql`SELECT texto, id FROM "Tarefas" ORDER BY id`)
 
  return <RealList rows={rows as unknown as {id: number, texto: string}[]}/>
 
