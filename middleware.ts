8370937b5cd5b71039260be910b9771117d58744
João Guilherme Stumpf Fernandes <guist4@proton.me>
2026-06-06 23:55:46 -0300

    n sei sinceramente

--- middleware.ts
+++ middleware.ts
@@ -60,6 +60,18 @@ export async function middleware(request: NextRequest) {
   return supabaseResponse
 }
 
+
+
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - auth/callback (THE CALLBACK ROUTE)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|auth/callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 }
